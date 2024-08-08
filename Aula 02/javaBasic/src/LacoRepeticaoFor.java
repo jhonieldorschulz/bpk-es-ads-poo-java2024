2c4d27e9c4d91ae23997c47772daae201977cf98
jhonieldorschulz <jhonieldorschulz@gmail.com>
2024-08-08 18:50:58 -0300

    Commit do laço de repetição FOR

---
+++ Aula 02/javaBasic/src/LacoRepeticaoFor.java
@@ -0,0 +1,12 @@
+public class LacoRepeticaoFor {
+    public static void main(String[] args) {
+        // Inicialização do array de inteiros
+        int[] numeros = {10, 20, 30, 40, 50};
+
+        // Percorrendo o array usando o laço de repetição for
+        for (int i = 0; i < numeros.length; i++) {
+            System.out.println("Elemento no índice " + i + ": " + numeros[i]);
+        }
+
+    }
+}
